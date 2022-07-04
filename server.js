e6ef6d76535b5af26ebd8d61722bfeeea18ca0de
Sabarna Biswas <58597364+Sabarna07@users.noreply.github.com>
2022-07-05 02:23:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({
+    origin : `${process.env.CLIENT_URL}`
+}));
 
 //cors
 //if (process.env.NODE_ENV === "development") {
